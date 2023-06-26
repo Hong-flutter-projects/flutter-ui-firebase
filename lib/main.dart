52b6e4d361f52640ae6fe9055cf51ae37bcdcee3
senghong <senghong@gmail.com>
2023-06-26 20:49:24 +0700

    first init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Container(
+        child: Center(
+          child: Text("Hello Bro"),
+        ),
+      )),
+    );
+  }
+}
